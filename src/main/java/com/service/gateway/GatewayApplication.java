10f56ffcb96d80efa8a2146438247a7fad38f5dc
zhaoxuan <zhaoxuan@jueeyaoair.com>
2020-07-07 17:35:23 +0800

    INIT

--- src/main/java/com/service/gateway/GatewayApplication.java
+++ src/main/java/com/service/gateway/GatewayApplication.java
@@ -2,6 +2,8 @@ package com.service.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -10,8 +12,13 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class GatewayApplication {
+public class GatewayApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GatewayApplication.class);
     }
 }
